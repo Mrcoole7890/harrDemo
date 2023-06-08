d83f8286867b9ee7654d70a5cfce1c3f5cf4a384
Cole A. Kendall <kendallcole7@gmail.com>
2023-06-07 20:18:45 -0400

    refactor code

--- product_scraper/spiders/demoSpider.py
+++ product_scraper/spiders/demoSpider.py
@@ -4,7 +4,10 @@ from urllib.parse import urljoin
 import re
 import mysql.connector
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 
 class AmazonSearchProductSpider(scrapy.Spider):
@@ -28,14 +31,26 @@ class AmazonSearchProductSpider(scrapy.Spider):
         'DOWNLOAD_DELAY': 1 # 2 seconds of delay
     }
 
+    discoverModeFlag = "discover"
+    auditModeFlag = "audit"
+    amazonKeywordSearchUrlFormat = "https://www.amazon.com/s?k={}&page={}"
+
+    def getDBConnection(self):
+        return mysql.connector.connect(
+                host=os.getenv("dbHostname"),
+                user=os.getenv("dbUsername"),
+                password=os.getenv("dbPassword"),
+                database=os.getenv("dbName")
+            )
+
     def start_requests(self):
 
-        if self.mode == "discover":
+        if self.mode == self.discoverModeFlag:
             keyword_list = ['laptops']
             for keyword in keyword_list:
-                amazon_search_url = f'https://www.amazon.com/s?k={keyword}&page=1'
+                amazon_search_url = amazonKeywordSearchUrlFormat.format(keyword,1)
                 yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': 1}, headers=self.HEADERS)
-        elif self.mode == "audit":
+        elif self.mode == self.auditModeFlag:
             for urls in self.getUrlList():
                 yield scrapy.Request(url=urls, callback=self.parse_product_data, headers=self.HEADERS)
 
@@ -43,108 +58,95 @@ class AmazonSearchProductSpider(scrapy.Spider):
         page = response.meta['page']
         keyword = response.meta['keyword'] 
 
+        cssPathPerListedProduct = "div.s-result-item[data-component-type=s-search-result]"
+        cssPathPerLinkOfListedProduct = "h2>a::attr(href)"
+        amazonHTTPAdress = "https://www.amazon.com/"
+        cssPathToNumberOfPagesAvailible = '//*[contains(@class, "s-pagination-item")][not(has-class("s-pagination-separator"))]/text()'
+
         ## Discover Product URLs
-        search_products = response.css("div.s-result-item[data-component-type=s-search-result]")
+        search_products = response.css(cssPathPerListedProduct)
         for product in search_products:
-            relative_url = product.css("h2>a::attr(href)").get()
-            product_url = urljoin('https://www.amazon.com/', relative_url).split("?")[0]
+            relative_url = product.css(cssPathPerLinkOfListedProduct).get()
+            product_url = urljoin(amazonHTTPAdress, relative_url).split("?")[0]
             yield scrapy.Request(url=product_url, callback=self.parse_product_data, meta={'keyword': keyword, 'page': page})
             
         ## Get All Pages
         if page == 1:
-            available_pages = response.xpath(
-                '//*[contains(@class, "s-pagination-item")][not(has-class("s-pagination-separator"))]/text()'
-            ).getall()
-
+            available_pages = response.xpath(cssPathToNumberOfPagesAvailible).getall()
             last_page = available_pages[-1]
             for page_num in range(2, int(last_page)):
-                amazon_search_url = f'https://www.amazon.com/s?k={keyword}&page={page_num}'
+                amazon_search_url = amazonKeywordSearchUrlFormat.format(keyword,page)
                 yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': page_num})
 
     def parse_product_data(self, response):
-        asin = response.url.split("/")[4].replace("?th=1", "") if self.mode == "audit" else response.url.split("/")[5]
-        name = response.css("#productTitle::text").get("").strip()
-        price = response.css('.a-price span[aria-hidden="true"] ::text').get("")
-        img = response.css('#ivLargeImage > img::attr(src)').get("")
+
+        cssPathOfProductName = "#productTitle::text"
+        cssPathOfPrice = '.a-price span[aria-hidden="true"] ::text'
+        cssPathOfProductImage = '#ivLargeImage > img::attr(src)'
+        cssPathOfPrice2 = '.a-price .a-offscreen ::text'
+        sqlInsertNewEntry = "INSERT INTO awslistings (asin, price, name) VALUES ({}, {}, {})"
+        sqlSelectAllASINawsListings = "SELECT asin FROM awslistings WHERE asin='{}'"
+        sqlSelectAllPricwawsListings = "SELECT price FROM awslistings WHERE asin='{}'"
+        noResultsForProductWithASIN = "AHHHHHHHHHHHHHHHHHHH {} is not found"
+        priceDifferenceLog = "\n\n\n\n\n\n\n {} : {} \n\n\n\n\n\n\n"
+
+        asin = response.url.split("/")[4].replace("?th=1", "") if self.mode == self.auditModeFlag else response.url.split("/")[5]
+        name = response.css(cssPathOfProductName).get("").strip()
+        price = response.css(cssPathOfPrice).get("")
+        img = response.css(cssPathOfProductImage).get("")
         if not price:
-            price = response.css('.a-price .a-offscreen ::text').get("")
-
-
+            price = response.css(cssPathOfPrice2).get("")
         yield {
             "name": name,
             "price": price,
             "ASIN": asin
         }
 
-        if self.mode != "audit":
-            mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="password",
-                database="flipper"
-            )
-
+        if self.mode != self.auditModeFlag:
+            mydb = self.getDBConnection()
             mycursor = mydb.cursor()
-
-            sql = "INSERT INTO awslistings (asin, price, name) VALUES (%s, %s, %s)"
-            val = (asin, price, name)
-            mycursor.execute(sql, val)
+            mycursor.execute(sqlInsertNewEntry.format(asin, price, name))
             mydb.commit()
             mydb.close()
 
         else:
-            mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="password",
-                database="flipper"
-            )
-
+            mydb = self.getDBConnection()
             mycursor = mydb.cursor()
 
-            mycursor.execute("SELECT asin FROM awslistings WHERE asin='{}'".format(asin))
-
+            mycursor.execute(sqlSelectAllASINawsListings.format(asin))
             asinFromDB = mycursor.fetchone()
             if asinFromDB == None:
-                print("AHHHHHHHHHHHHHHHHHHH {} is not found".format(asin))
+                print(noResultsForProductWithASIN.format(asin))
                 return
-            
-            mycursor.execute("SELECT price FROM awslistings WHERE asin='{}'".format(asin))
-
+            mycursor.execute(sqlSelectAllPricwawsListings.format(asin))
             priceFromDB = mycursor.fetchone()
             if priceFromDB == None:
-                print("AHHHHHHHHHHHHHHHHHHH {} is not found".format(asin))
+                print(noResultsForProductWithASIN.format(asin))
                 return
 
             print(price)
 
-            if float(priceFromDB[0][1:].replace(",", "")) > float(price[1:].replace(",", "")):
-                print("\n\n\n\n\n\n\n {} : {} \n\n\n\n\n\n\n".format(price, priceFromDB[0]))
+            if self.isNewPriceLessThanOldOne(float(priceFromDB[0][1:].replace(",", "")), float(price[1:].replace(",", ""))):
+                print(priceDifferenceLog.format(price, priceFromDB[0]))
                 mes = [name, str(float(priceFromDB[0][1:].replace(",", "")) - float(price[1:].replace(",", ""))), response.url, img]
                 self.sendMessageToDiscord(mes)
 
             
             mydb.close()
             
-            
-    #"http://www.amazon.com/dp/B07XKXQL79"
     def getUrlList(self):
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="password",
-            database="flipper"
-        )
 
+        selectAllAsin = "SELECT asin FROM awslistings"
+        amazonURLByAsin = "http://www.amazon.com/dp/{}"
+
+        mydb = self.getDBConnection()
         mycursor = mydb.cursor()
-
-        mycursor.execute("SELECT asin FROM awslistings")
-
+        mycursor.execute(selectAllAsin)
         myresult = mycursor.fetchall()
         mydb.close()
         finalresult = []
         for x in myresult:
-            finalresult.append("http://www.amazon.com/dp/{}".format(x[0]))
+            finalresult.append(amazonURLByAsin.format(x[0]))
 
         return finalresult
 
@@ -154,7 +156,8 @@ class AmazonSearchProductSpider(scrapy.Spider):
 
 
     def sendMessageToDiscord(self, message):
-        
+        # The API endpoint to communicate with
+        url_post = "https://discord.com/api/webhooks/1109646951793840158/6GdjC3ME3yvDJh8k1ou4EFHRJMwZox_She0bWxPjLyXrKEgJSd0W4yldHmkWyvNY_dPN"
         new_data = {
             "embeds": [
                 {
@@ -170,11 +173,9 @@ class AmazonSearchProductSpider(scrapy.Spider):
                 }
             ]
         }
-
-        # The API endpoint to communicate with
-        url_post = "https://discord.com/api/webhooks/1109646951793840158/6GdjC3ME3yvDJh8k1ou4EFHRJMwZox_She0bWxPjLyXrKEgJSd0W4yldHmkWyvNY_dPN"
-
         # A POST request to tthe API
         post_response = requests.post(url_post, json=new_data)
 
-        
+    def isNewPriceLessThanOldOne(self, value1, value2):
+        return value1 > value2
+        #return float(priceFromDB[0][1:].replace(",", "")) > float(price[1:].replace(",", ""))
